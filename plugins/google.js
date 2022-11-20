7cc83886bc6eb102b9a4b9f78f49e6a8b4e7e770
Ryumosy <114341726+shinmosy@users.noreply.github.com>
2022-11-20 17:59:30 +0700

    Update google.js

--- plugins/google.js
+++ plugins/google.js
@@ -24,7 +24,7 @@ let handler = async (m, { conn, command, args }) => {
 }
 handler.help = ['google', 'googlef'].map(v => v + ' <pencarian>')
 handler.tags = ['internet']
-handler.command = /^googlef?$/i
+handler.command = /^ggl|googlef?$/i
 
 
 export default handler
